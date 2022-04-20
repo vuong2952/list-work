f15377eb3df12a1b406994dc94ab03dc1a05db21
LVV <higo2952@gmail.com>
2022-04-20 09:05:33 +0700

    push

---
+++ src/config/status.js
@@ -0,0 +1,32 @@
+export default {
+    "undefined" => [
+        "code" => 'nstarted',
+        "color" => 'gray',
+        "text" => 'Chưa bắt đầu'
+    ],
+    "nstarted" => [
+        "code" => 'nstarted',
+        "color" => 'gray',
+        "text" => 'Chưa bắt đầu'
+    ],
+    "started" => [
+        "code" => 'started',
+        "color" => '#0dcaf0',
+        "text" => 'Chưa bắt đầu'
+    ],
+    "paused" => [
+        "code" => 'paused',
+        "color" => '#ffc107',
+        "text" => 'Chưa bắt đầu'
+    ],
+    "error" => [
+        "code" => 'error',
+        "color" => '#dc3545',
+        "text" => 'Chưa bắt đầu'
+    ],
+    "finished" => [
+        "code" => 'finished',
+        "color" => '#28a745',
+        "text" => 'Chưa bắt đầu'
+    ],
+}
